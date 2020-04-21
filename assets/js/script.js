58235a35c873c6e277dda1c626f7e413d1593822
Robin Collins <info@robincollins.co.uk>
2020-04-21 13:26:33 +0000

    updated JavaScript

---
+++ assets/js/script.js
@@ -0,0 +1,143 @@
+// Character Strength 
+
+const cards = [
+  {
+    'name': 'Catboy',
+    'image_link': 'assets/images/Catboy_card.png',
+    'strength': 10,
+  },
+  {
+    'name': 'Bearz',
+    'image_link': 'assets/images/Bear_card.png',
+    'strength': 100,
+  },
+  {
+    'name': 'Mook',
+    'image_link': 'assets/images/Mook_cards.png',
+    'strength': 3,
+  },
+  {
+    'name': 'Cat Castle',
+    'image_link': 'assets/images/Catcastle_card.png',
+    'strength': 10,
+  },
+  {
+    'name': 'Catdog',
+    'image_link': 'assets/images/Catdog_card.png',
+    'strength': 7,
+  },
+  {
+    'name': 'Daah',
+    'image_link': 'assets/images/Daadee_card.png',
+    'strength': 8,
+  },
+  {
+    'name': 'Darbin',
+    'image_link': 'assets/images/Darbin_card.png',
+    'strength': 10,
+  },
+  {
+    'name': 'Frog',
+    'image_link': 'assets/images/Frog_card.png',
+    'strength': 5,
+  },
+  {
+    'name': 'Geko',
+    'image_link': 'assets/images/Geko_card.png',
+    'strength': 6,
+  },
+  {
+    'name': 'Glammy',
+    'image_link': 'assets/images/Glammy_card.png',
+    'strength': 7,
+  },
+  {
+    'name': 'Karr',
+    'image_link': 'assets/images/Karr_cards.png',
+    'strength': 8,
+  },
+  {
+    'name': 'Kay-kee',
+    'image_link': 'assets/images/Kaykee_cards.png',
+    'strength': 2,
+  },
+  {
+    'name': 'Knotting-Mouth',
+    'image_link': 'assets/images/Knottingmouth_card.png',
+    'strength': 10,
+  },
+  {
+    'name': 'Mar-Mee-duc',
+    'image_link': 'assets/images/Marmee_card.png',
+    'strength': 5,
+  },
+  {
+    'name': 'Moo',
+    'image_link': 'assets/images/Moo_card.png',
+    'strength': 10,
+  },
+  {
+    'name': 'Mum-Mers',
+    'image_link': 'assets/images/Mummers_card.png',
+    'strength': 9,
+  },
+  {
+    'name': 'Pee-Peels',
+    'image_link': 'assets/images/Peapeel_card.png',
+    'strength': 10,
+  },
+  {
+    'name': 'Peet',
+    'image_link': 'assets/images/Peet_card.png',
+    'strength': 9,
+  },
+  {
+    'name': 'Raaaa',
+    'image_link': 'assets/images/Raaaaa_cards.png',
+    'strength': 10,
+  },
+  {
+    'name': 'Rory Dragon',
+    'image_link': 'assets/images/Rory_Dragon_card.png',
+    'strength': 10,
+  },
+  {
+    'name': 'T-Ruck',
+    'image_link': 'assets/images/Truck_card.png',
+    'strength': 5,
+  },
+  {
+    'name': 'Twoo',
+    'image_link': 'assets/images/Twoo_card.png',
+    'strength': 7,
+  },
+  {
+    'name': 'Upsteps',
+    'image_link': 'assets/images/Upsteps_card.png',
+    'strength': 9,
+  },
+  {
+    'name': 'Weewoo',
+    'image_link': 'assets/images/Weewoo_card.png',
+    'strength': 7,
+  },
+  {
+    'name': 'Whistle',
+    'image_link': 'assets/images/Whistle_card.png',
+    'strength': 7,
+  },
+   {
+    'name': 'Winky-Face',
+    'image_link': 'assets/images/Winky_Face_card.png',
+    'strength': 4,
+  },
+
+]
+
+
+function showCard(cardID){
+	//Flip the card
+	let card_div = document.getElementById(cardID)
+	// Then swap class back to front
+
+}
